a4f0f7ea64192b37c43f343e78994c68a1003192
chanhi <61691088+chanhi@users.noreply.github.com>
2022-07-05 19:16:43 +0900

    start making movie app

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
-//import Test from './test';
-import React from "react";
-//import PropTypes from "prop-types"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
-```
-Food.propTypes = { //propTypes ProTypes 아님 주의
-  name: PropTypes.string.isRequired,
-  star: PropTypes.number.isRequired
-};
-```
-class App extends React.Component {
-  state = {
-    count: 0
-  };
-  add = () => {
-    this.setState({count:1});
-  }
-  minus = () => {
-    this.setState({count:-1});
-  }
-  render() {
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Add</button>
-      </div>
-    );
-  }
-}
-
-```
 function App() {
   return (
-    <div className="App">
-      <Test />
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
-```
+
 export default App;
--- src/test.js
+++ src/test.js
@@ -1,10 +1,64 @@
-
 function Test() {
-    return (
-      <div className="App">
-        <h2>This is other componet</h2>
-      </div>
-    );
-  }
-  
-  export default Test;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    //[value, ...list] -> list앞에 ...을 붙이면 리스트 안에 리스트가 아니라 리스트안의 내용만 들어감
+    setToDo("");
+    console.log(toDos);
+  };
+  return (
+    <div className="App">
+      <h1>My To Dos : {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do ..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <Coin />
+    </div>
+  );
+}
+
+function Coin() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>The Coins</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <ul>
+        {coins.map((coin, index) => (
+          <li>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Test;
